9f66261b3359cfadf4b289353992319bc760eb46
Bagas Wahyu Hidayah <bagaswh01@gmail.com>
2021-11-09 11:30:18 +0700

    add sandbox

---
+++ tests/sandbox.js
@@ -0,0 +1,56 @@
+const { LiteSpeedConf } = require('../index');
+
+const config = `docRoot                   $VH_ROOT/client
+
+errorlog /var/log/lsws/vhost_main/error.log {
+  useServer               0
+  logLevel                ERROR
+  rollingSize             10M
+  keepDays                30
+  compressArchive         1
+}
+
+accesslog /var/log/lsws/vhost_main/access.log {
+  useServer               0
+  rollingSize             10M
+  keepDays                30
+  compressArchive         1
+}
+
+context /foo {
+  location                $DOC_ROOT/foo/prod
+  allowBrowse             1
+
+  rewrite  {
+    enable                1
+
+  }
+  addDefaultCharset       off
+
+  phpIniOverride  {
+
+  }
+}
+
+rewrite  {
+  enable                  1
+  autoLoadHtaccess        1
+}
+`;
+
+const liteSpeedConf = new LiteSpeedConf(config);
+
+liteSpeedConf.conf.add('context', '/baz', {
+  location: '$DOC_ROOT/baz/prod',
+  allowBrowse: 1,
+  rewrite: {
+    enable: 1,
+  },
+  addDefaultCharset: 'off',
+  phpIniOverride: {},
+  nested: {
+    foo: 'baz',
+  },
+});
+
+console.log(liteSpeedConf.toString());
