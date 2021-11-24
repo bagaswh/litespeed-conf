b460dac530b6032bfa27b7b3891d423bc9b2e32f
bagaswh <devops@botika.online>
2021-11-24 18:56:00 +0700

    ensure remove works properly

--- tests/conf/conf.test.js
+++ tests/conf/conf.test.js
@@ -2,20 +2,20 @@ const { LiteSpeedConf } = require('../../index');
 
 const config = ``;
 
-const liteSpeedConf = new LiteSpeedConf(config);
+const litespeed = new LiteSpeedConf(config);
 
 describe('Conf', () => {
   test('add node', () => {
-    liteSpeedConf.conf.add('key', 'value');
-    expect(liteSpeedConf.tree.get('key').value).toBe('value');
-    liteSpeedConf.conf.add('foo', 'bar', {
+    litespeed.conf.add('key', 'value');
+    expect(litespeed.tree.get('key').value).toBe('value');
+    litespeed.conf.add('foo', 'bar', {
       baz: 'qux',
     });
-    expect(liteSpeedConf.tree.get('foo').children).toHaveLength(1);
+    expect(litespeed.tree.get('foo').children).toHaveLength(1);
   });
 
   test('set node value', () => {
-    liteSpeedConf.conf.add('rewrite', '', {
+    litespeed.conf.add('rewrite', '', {
       foo: 'bar',
       qux: {
         baz: 'perpendicular',
@@ -25,32 +25,26 @@ describe('Conf', () => {
       },
     });
 
-    liteSpeedConf.conf.get('rewrite').get('foo').set('halo');
-    expect(liteSpeedConf.conf.get('rewrite').get('foo').getValue()).toBe(
-      'halo'
-    );
+    litespeed.conf.get('rewrite').get('foo').set('halo');
+    expect(litespeed.conf.get('rewrite').get('foo').getValue()).toBe('halo');
 
-    liteSpeedConf.conf
-      .get('rewrite')
-      .get('qux')
-      .get('baz')
-      .set('something_else');
-    expect(
-      liteSpeedConf.conf.get('rewrite').get('qux').get('baz').getValue()
-    ).toBe('something_else');
+    litespeed.conf.get('rewrite').get('qux').get('baz').set('something_else');
+    expect(litespeed.conf.get('rewrite').get('qux').get('baz').getValue()).toBe(
+      'something_else'
+    );
   });
 
   test('remove node', () => {
-    liteSpeedConf.conf.remove('key', 'value');
-    expect(liteSpeedConf.conf.get('key')).toBe(null);
+    litespeed.conf.remove('key', 'value');
+    expect(litespeed.conf.get('key')).toBe(null);
 
-    liteSpeedConf.conf.remove('key', 'value');
-    expect(liteSpeedConf.conf.get('foo').node.children).toHaveLength(1);
+    litespeed.conf.remove('key', 'value');
+    expect(litespeed.conf.get('foo').node.children).toHaveLength(1);
 
-    liteSpeedConf.conf.remove('foo');
-    expect(liteSpeedConf.conf.get('foo')).toBe(null);
+    litespeed.conf.remove('foo');
+    expect(litespeed.conf.get('foo')).toBe(null);
 
-    liteSpeedConf.conf.add('context', '/wus-wus', {
+    litespeed.conf.add('context', '/wus-wus', {
       foo: 'bar',
       qux: {
         baz: 'perpendicular',
@@ -60,17 +54,28 @@ describe('Conf', () => {
       },
     });
 
-    liteSpeedConf.conf.get('context', '/wus-wus').remove('qux');
-    expect(liteSpeedConf.conf.get('context', '/wus-wus').get('qux')).toBe(null);
+    litespeed.conf.get('context', '/wus-wus').remove('qux');
+    expect(litespeed.conf.get('context', '/wus-wus').get('qux')).toBe(null);
     expect(
-      liteSpeedConf.conf.get('context', '/wus-wus').get('foo').getValue()
+      litespeed.conf.get('context', '/wus-wus').get('foo').getValue()
     ).toBe('bar');
 
-    liteSpeedConf.conf.get('context', '/wus-wus').remove();
-    expect(liteSpeedConf.conf.get('context', '/wus-wus')).toBe(null);
+    litespeed.conf.get('context', '/wus-wus').remove();
+    expect(litespeed.conf.get('context', '/wus-wus')).toBe(null);
+
+    // removing specific key-value
+    litespeed.conf.add('foo', 'bar');
+    litespeed.conf.add('foo', 'baz');
+    litespeed.conf.add('foo', 'qux');
+    litespeed.conf.get('foo', 'baz').remove();
+    expect(litespeed.conf.get('foo', 'baz')).toBe(null);
+    litespeed.conf.get('foo', 'qux').remove();
+    expect(litespeed.conf.get('foo', 'qux')).toBe(null);
+    litespeed.conf.get('foo', 'bar').remove();
+    expect(litespeed.conf.get('foo', 'bar')).toBe(null);
   });
 
   test('toString', () => {
-    console.log(liteSpeedConf.toString());
+    console.log(litespeed.toString());
   });
 });
