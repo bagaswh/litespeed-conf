ab8091ad0cdf9b5f6f6d240cf5442595ed94486c
Bagas Wahyu Hidayah <bagaswh00@gmail.com>
2022-04-08 22:17:20 +0700

    Update common.js

--- tests/common.js
+++ tests/common.js
@@ -16,31 +16,6 @@ accesslog /var/log/lsws/vhost_main/access.log {
   compressArchive         1
 }
 
-context /bank-indonesia {
-  location                $DOC_ROOT/bank-indonesia/prod
-  allowBrowse             1
-
-  rewrite  {
-    enable                1
-
-  }
-  addDefaultCharset       off
-
-  phpIniOverride  {}
-}
-
-context /lgi {
-  location                $DOC_ROOT/lgi/prod
-  allowBrowse             1
-
-  rewrite  {
-   enable                1
-  }
-  addDefaultCharset       off
-
-  phpIniOverride  {}
-}
-
 rewrite             {
   enable                  1
   autoLoadHtaccess        1
